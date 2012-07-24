544669fe7671f70996161159d7cfdfbd21b3fa7a
revender <gabriel.rev94@gmail.com>
2012-07-24 21:48:57 +0300

    iti dau un caine

---
+++ src/main/Dog.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Dog {
+	public String name;
+	
+	public void spuneNume() {
+		System.out.println("Numele meu este: "+this.name);
+	}
+}
---
+++ src/main/DogTester.java
@@ -0,0 +1,14 @@
+package main;
+
+public class DogTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		
+		d.spuneNume();
+	}
+
+}
